69f6bad07adbf5c98753b6144b213c51ed67f219
Jörg Schönberger <office@webchills.at>
2022-12-18 19:19:15 +0100

    Version 1.0.2

--- NEUE DATEIEN/includes/languages/english/modules/shipping/zones5.php
+++ NEUE DATEIEN/includes/languages/english/modules/shipping/zones5.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2022 Zen Cart Development Team
- * Zen Cart German Version - www.zen-cart-pro.at
- * @copyright Portions Copyright 2003 osCommerce
- * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- 
- * @version $Id: zones5.php 2012-02-22 08:45:57Z webchills $
- */
-
 define('MODULE_SHIPPING_ZONES5_TEXT_TITLE', 'Zone Rates');
 define('MODULE_SHIPPING_ZONES5_TEXT_DESCRIPTION', 'Zone Based Rates');
 define('MODULE_SHIPPING_ZONES5_TEXT_WAY', 'Shipping to');
--- NEUE DATEIEN/includes/languages/german/modules/shipping/zones2.php
+++ NEUE DATEIEN/includes/languages/german/modules/shipping/zones2.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2022 Zen Cart Development Team
- * Zen Cart German Version - www.zen-cart-pro.at
- * @copyright Portions Copyright 2003 osCommerce
- * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- 
- * @version $Id: zones2.php 2012-02-21 17:45:57Z webchills $
- */
-
 define('MODULE_SHIPPING_ZONES2_TEXT_TITLE','Versandkosten nach Zonen');
 define('MODULE_SHIPPING_ZONES2_TEXT_DESCRIPTION','Versandkosten nach Zonen');
 define('MODULE_SHIPPING_ZONES2_TEXT_WAY','nach');
